b910bc4725fb163bc67a87cbadfa3d47a7954e8c
Kelvin Chappell <kelvin.chappell@guardian.co.uk>
2013-09-18 09:41:00 +0100

    wip

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,9 +3,13 @@ package controllers
 import play.api.mvc._
 
 object Application extends Controller {
-  
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
+  def board = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+
 }
