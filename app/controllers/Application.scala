e70c78a499548c3b9784ad31367a19c722b8421a
Kelvin Chappell <kelvin.chappell@guardian.co.uk>
2013-11-07 09:42:08 +0000

    wip

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,7 +3,7 @@ package controllers
 import play.api.mvc._
 import model.service.{ToodledoService, TaskRepository}
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.Try
+import scala.util.{Success, Failure, Try}
 import scala.concurrent.Future
 import model.Context
 
@@ -16,11 +16,20 @@ object Application extends Controller {
   }
 
   def contexts = Action.async {
-    val contexts2 = Try(TaskRepository.getContexts)
-    contexts2.
-    val contexts1 = TaskRepository.getContexts
-    contexts1 map {
-      c => Ok(views.html.contexts(c))
+    val contexts2 = TaskRepository.getContexts
+    contexts2 map {
+      x =>
+        val w = Try(x)
+        w match {
+          case Success(c) => {
+            Ok(views.html.contexts(c))
+          }
+          case Failure(ToodledoService.Exception(id, description)) => {
+            InternalServerError(views.html.exception(ToodledoService.Exception(id, description)))
+          }
+          case Failure(e) =>
+            throw e
+        }
     }
   }
 
